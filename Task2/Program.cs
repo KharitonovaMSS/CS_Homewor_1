6ea9cec9fd7db36421d712a1e1fd4cffbd1de923
Maria <mineeva_maria@mail.ru>
2022-11-12 19:38:30 +0300

    Найдено большее число

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.WriteLine("Максимальное" + a);
+if (b > a)
+    Console.WriteLine("Максимальное" + b);
+if (a == b)
+    Console.WriteLine("равны");
